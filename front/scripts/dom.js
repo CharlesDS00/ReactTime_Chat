9471d24f8ba56e8dcbe0c8d398dab9369bb1160e
Charles de Souza <desouzacharles5@gmail.com>
2022-04-27 12:25:09 +0200

    add send, date and css
    Ajout de la différenciation et des dates d'envoies
    Et css du site

--- front/scripts/dom.js
+++ front/scripts/dom.js
@@ -3,18 +3,34 @@ const main = document.querySelector('main')
 /** @param {Record<string, string>} data */
 export function appendMessage(data) {
   const msgEl = document.createElement('div')
-  
+
   msgEl.classList.add('message')
 
   const pseudoSpan = document.createElement('span')
   pseudoSpan.textContent = data.pseudo
   msgEl.append(pseudoSpan)
 
+  const time = document.createElement('span')
+  time.classList.add('dateMessage')
+  time.textContent = new Date().toLocaleString()
+  msgEl.append(time)
+  
   const bodyP = document.createElement('p')
   bodyP.textContent = data.body
   msgEl.append(bodyP)
 
   main?.appendChild(msgEl)
   main?.scrollTo(0, main.scrollHeight)
+
+ /* const sender = document.querySelector('#pseudo')?.value
+  sender.p 
+  msgEl.classList.add(data.pseudo === sender ? 'send' : 'recieve')
+*/
+const verifpseudo = (data.pseudo === localStorage.getItem("pseudo")) ;
+    if (verifpseudo){
+    msgEl.dataset.id = data.id; 
+    msgEl.classList.add("send");
+    }
 }
+
 
